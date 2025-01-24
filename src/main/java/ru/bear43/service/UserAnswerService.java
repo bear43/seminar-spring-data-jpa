d3be3180b2c8a2d659892380c375a4600ad04fc5
bear43 <bigwilly4307@gmail.com>
2025-01-25 01:42:58 +0300

    Drop unused service layer

--- src/main/java/ru/bear43/service/UserAnswerService.java
+++
@@ -1,6 +0,0 @@
-package ru.bear43.service;
-
-public interface UserAnswerService {
-    void answer(Long userId, Long formId, Long questionId, Long answerId);
-    void remove(Long userId, Long formId, Long questionId);
-}
